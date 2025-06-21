af59ff1493ed500f538aace7421fe5937020a8f7
Feroze Mohammad <45548249+ferozem@users.noreply.github.com>
2025-06-21 08:30:33 +0530

    consolidate static data

---
+++ src/data/promoBanners.js
@@ -0,0 +1,17 @@
+export const promoBanners = [
+  {
+    title: "Flat 50% OFF!",
+    code: "QUICKEATS50",
+    bg: "bg-gradient-to-r from-red-500 to-orange-400"
+  },
+  {
+    title: "Free Delivery",
+    code: "FREEDEL",
+    bg: "bg-gradient-to-r from-green-500 to-teal-400"
+  },
+  {
+    title: "20% Cashback",
+    code: "CASHBACK20",
+    bg: "bg-gradient-to-r from-indigo-500 to-purple-500"
+  }
+];
